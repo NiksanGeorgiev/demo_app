b9819eb6a385c06c2e3896624d1a33596cbe275e
NiksanGeorgiev <nikolaypavgr@gmail.com>
2024-12-16 12:06:56 +0100

    Added automatic versioning

--- android/scripts/flutter_build_android.sh
+++ android/scripts/flutter_build_android.sh
@@ -3,45 +3,35 @@
 # Fail this script if any subcommand fails.
 set -e
 
-# # Function that shows usage information
-# usage()
-# {
-#     echo ""
-#     echo "Usage: $0 -c settings.prod"
-#     echo "  -c | --config-file              File containing the Flutter compile time configuration settings."
-#     echo "  -e | --environment              The environment for which the application should be build. Possible values are 'dev' or 'development', 'stg' or 'staging' and 'prod' or 'production'."   
-#     echo "  --launcher-icon-config-file     The file containing the configuration needed to generate the launch icon. By default the 'flutter_launcher_icons.yaml' is used."
-#     echo "  -h | --help                     Shows this help text."
-# }
-
-# LAUNCHER_ICON_CONFIG="flutter_launcher_icons.yaml"
-
-# # Parse arguments
-# while [ "$1" != "" ]; do
-#     case $1 in
-#         -c | --config-file)             shift
-#                                         CONFIG_FILE=$1
-#                                         ;;
-#         -e | --environment)             shift
-#                                         ENVIRONMENT=$1
-#                                         ;;
-#         --launcher-icon-config-file)    shift
-#                                         LAUNCHER_ICON_CONFIG=$1
-#                                         ;;
-#         -h | --help )                   usage
-#                                         exit
-#                                         ;;
-#         * )                             usage
-#                                         exit 1
-#     esac
-#     shift
-# done
-
 # Change working directory to the project root 
 cd ../../
 
 # Make sure we work in a clean environment.
 flutter clean
+
+# Get the current version from pubspec.yaml
+current_version=$(grep '^version:' pubspec.yaml | awk '{print $2}')
+
+# Get the previous version from the last commit
+previous_version=$(git show HEAD~1:pubspec.yaml | grep '^version:' | awk '{print $2}')
+
+# Automatic version incrementation when version isn't changed manualy in pubspec.yaml
+if [ "$current_version" == "$previous_version" ]; then
+    # Extract the version code (the part after the '+')
+    current_version_code=$(echo $current_version | awk -F'+' '{print $2}')
+
+    # Increment the version code by 1
+    new_version_code=$((current_version_code + 1))
+
+    # Extract the version name (the part before the '+')
+    current_version_name=$(echo $current_version | awk -F'+' '{print $1}')
+
+    # Create the new version string
+    new_version="$current_version_name+$new_version_code"
+
+    # Update the pubspec.yaml file with the new version
+    sed -i "s/^version: .*/version: $new_version/" pubspec.yaml
+fi
 
 # Build the Flutter application.
 flutter build appbundle --release
